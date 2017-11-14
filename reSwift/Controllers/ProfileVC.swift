9de63944a1ddebf41a3d92ee701416bf2545908d
Roman Stolyarchuk <roman.stoliarchuk@cyberchisel.com>
2017-11-14 18:02:06 +0200

    Update usability of mock navigation of the project (search,profile,trending)

--- reSwift/Controllers/ProfileVC.swift
+++ reSwift/Controllers/ProfileVC.swift
@@ -15,6 +15,8 @@ class ProfileVC: UIViewController {
     @IBOutlet weak var profileFullName: UILabel!
     @IBOutlet weak var followersCount: UILabel!
     @IBOutlet weak var followingCount: UILabel!
+    @IBOutlet weak var repoBtn: UIButton!
+    @IBOutlet weak var eventsBtn: UIButton!
     
     @IBOutlet var profileTopBorderedViews: [UIView]!
     @IBOutlet var profileBottomBorderedViews: [UIView]!
@@ -22,20 +24,34 @@ class ProfileVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         profileImage.layer.borderColor = UIColor.white.cgColor
-        profileTopBorderedViews.map { v in
+        profileTopBorderedViews.forEach { v in
             v.layer.borderColor = UIColor.white.withAlphaComponent(0.12).cgColor
         }
-        profileBottomBorderedViews.map { v in
+        profileBottomBorderedViews.forEach { v in
             v.layer.borderColor = UIColor(white: 233.0 / 255.0, alpha: 1.0).cgColor
         }
+        
+        repoBtn.addTarget(self, action: #selector(repoAction), for: .touchUpInside)
+        eventsBtn.addTarget(self, action: #selector(eventsAction), for: .touchUpInside)
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func repoAction(_ sender: UIButton) {
+        if let reposVC = UIStoryboard(name: "StarredVC", bundle: nil).instantiateViewController(withIdentifier: "StarredVC") as? StarredVC {
+            reposVC.titleText = "Repositories"
+            reposVC.dataType = .repositories
+            self.navigationController?.pushViewController(reposVC, animated: true)
+        }
+    }
+    
+    @objc func eventsAction(_ sender: UIButton) {
+        if let eventsVC = UIStoryboard(name: "StarredVC", bundle: nil).instantiateViewController(withIdentifier: "StarredVC") as? StarredVC {
+            eventsVC.titleText = "Events"
+            eventsVC.dataType = .events
+            self.navigationController?.pushViewController(eventsVC, animated: true)
+        }
     }
 }
--- reSwift/Controllers/RepositoriesController.swift
+++ reSwift/Controllers/RepositoriesController.swift
@@ -51,15 +51,7 @@ class RepositoriesController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backAction(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
     }
-    */
-
 }
